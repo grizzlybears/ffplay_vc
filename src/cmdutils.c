78bc946b058af9031a666f5e1ab9ced31bbd03b8
vampirictutor <vampirictutor@gmail.com>
2021-02-20 13:14:05 +0800

    fit ffmpeg 4.3.x

--- src/cmdutils.c
+++ src/cmdutils.c
@@ -202,14 +202,13 @@ void show_help_options(const OptionDef *options, const char *msg, int req_flags,
 
 void show_help_children(const AVClass *class, int flags)
 {
-    void *iter = NULL;
-    const AVClass *child;
+    const AVClass *child = NULL;
     if (class->option) {
         av_opt_show2(&class, NULL, flags, 0);
         printf("\n");
     }
 
-    while (child = av_opt_child_class_iterate(class, &iter))
+    while (child = av_opt_child_class_next(class, child))
         show_help_children(child, flags);
 }
 
